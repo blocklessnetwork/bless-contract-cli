383b740a66974fcb05deaf007dd4bce07ce3eb07
Joinhack <joinhack@gmail.com>
2025-06-25 16:47:53 +0800

    init

--- command/reg_set_deadline.js
+++ command/reg_set_deadline.js
@@ -1,8 +1,46 @@
 const { Command, Argument } = require("commander");
+const anchor = require("@coral-xyz/anchor");
+const chalk = require("chalk");
+const { getBlsRegisterClient, getPath, readKeypair } = require("./utils");
 
-const registrySetDeadlineCommand = new Command("deadline")
+const registerSetDeadlineCommand = new Command("deadline")
   .option(
     "--cluster <cluster>",
     "solana cluster: mainnet, testnet, devnet, localnet, <custom>",
   )
-  .description("deactive: registry deactive wallet");
+  .description(
+    "deadline: set/show the deadline of the registration, if is null, show daedline",
+  );
+const deadline = new Argument(
+  "deadline",
+  "the deadline of registration, deafult: now() + n * 24hours",
+);
+deadline.required = false;
+registerSetDeadlineCommand
+  .addArgument(deadline)
+  .action(async (deadline, options) => {
+    options.cluster = options.cluster || "localnet";
+    options.signer = options.signer || getPath("~/.config/solana/id.json");
+    const keypair = readKeypair(options.signer);
+    const client = getBlsRegisterClient(options.cluster, keypair);
+    if (deadline != null) {
+      const now = new Date().getTime() / 1000;
+      if (parseInt(deadline) < now) {
+        deadline = now + 24 * 60 * 60 * deadline;
+      }
+      await client.nodeRegistrationClient.setRegistrationDeadline(
+        new anchor.BN(deadline),
+      );
+      console.log(chalk.green("registration set deadline success."));
+    } else {
+      const r = await client.nodeRegistrationClient.getNodeRegistration();
+      const deadline = new Date(r.registrationDeadline.toNumber() * 1000);
+      console.log(
+        chalk.green("registration deadline is :" + deadline.toISOString()),
+      );
+    }
+
+    process.exit(0);
+  });
+
+module.exports = registerSetDeadlineCommand;
--- command/register.js
+++ command/register.js
@@ -1,9 +1,11 @@
 const { Command, Argument } = require("commander");
 const regInitCommand = require("./reg_init");
+const registerSetDeadlineCommand = require("./reg_set_deadline");
 
 const registerCommand = new Command("register").description(
   "register: the operations for registration conract.",
 );
 registerCommand.addCommand(regInitCommand);
+registerCommand.addCommand(registerSetDeadlineCommand);
 
 module.exports = registerCommand;
