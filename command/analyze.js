72e31d995da22914d949d84dd0c331f33871146f
Joinhack <joinhack@gmail.com>
2025-06-26 16:57:05 +0800

    support analyze

--- command/analyze.js
+++ command/analyze.js
@@ -4,9 +4,12 @@ const anchor = require("@coral-xyz/anchor");
 const chalk = require("chalk");
 const { Keypair } = require("@solana/web3.js");
 const fs = require("node:fs");
-const analyzeCommand = new Command("analyze").description(
-  "analyze: analyze the register user assests",
-);
+const analyzeCommand = new Command("analyze")
+  .option(
+    "--pindex <page index>",
+    "page index: solana bless node page index, default: 0",
+  )
+  .description("analyze: analyze the register user assests");
 const file = new Argument(
   "whitelist",
   `the assests whitelist, the whitelist contents like {"TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA": 150}`,
@@ -18,10 +21,13 @@ analyzeCommand.addArgument(file).action(async (file, options) => {
   const client = getBlsRegisterClient(options.cluster, keypair);
   let rs = null;
   let pageIndex = 0;
-  const whiteList = null;
+  if (!isNaN(options.pindex)) {
+    pageIndex = parseInt(options.pindex);
+  }
+  let whiteList = null;
 
   if (file != null && fs.existsSync(file)) {
-    whiteList = JSON.parse(fs.readFileSync(path, "utf8"));
+    whiteList = JSON.parse(fs.readFileSync(file, "utf8"));
   }
 
   do {
@@ -38,12 +44,12 @@ analyzeCommand.addArgument(file).action(async (file, options) => {
         let print = true;
         if (whiteList != null) {
           const limit = whiteList[userToken.mint];
-          if (userToken > limit) print = true;
+          if (userToken.amount < limit) print = false;
         }
         if (print) {
           const mint = userToken.mint.toBase58();
           const amount = userToken.amount;
-          console.log(`|---- mint: ${mint} amount:${amount}`);
+          console.log(`        mint: ${mint} amount:${amount}`);
         }
       }
     }
