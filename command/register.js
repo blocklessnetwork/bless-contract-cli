ea54f3243f62a03369dcaf16bc1b0a330b61c4d1
Joinhack <joinhack@gmail.com>
2025-07-15 18:56:13 +0800

    the pending account and accept admin.

--- command/register.js
+++ command/register.js
@@ -3,6 +3,8 @@ const regInitCommand = require("./reg_init");
 const registerSetDeadlineCommand = require("./reg_set_deadline");
 const registerDoCommand = require("./reg_register");
 const registerSetPauseCommand = require("./reg_pause");
+const registerAcceptAdminCommand = require("./reg_accept_admin");
+const registerSetPendingAdminCommand = require("./reg_set_pending_admin");
 
 const registerCommand = new Command("register").description(
   "register: the operations for registration conract.",
@@ -11,5 +13,7 @@ registerCommand.addCommand(regInitCommand);
 registerCommand.addCommand(registerSetDeadlineCommand);
 registerCommand.addCommand(registerDoCommand);
 registerCommand.addCommand(registerSetPauseCommand);
+registerCommand.addCommand(registerAcceptAdminCommand);
+registerCommand.addCommand(registerSetPendingAdminCommand);
 
 module.exports = registerCommand;
