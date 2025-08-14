95633763e379125226351371db68cb7f327ba505
Joinhack <joinhack@gmail.com>
2025-08-14 11:41:52 +0800

    support the time accept admin

---
+++ command/time_accept_admin.js
@@ -0,0 +1,46 @@
+const { Command, Argument } = require("commander");
+const anchor = require("@coral-xyz/anchor");
+const chalk = require("chalk");
+const { WALLET_PATH } = require("../lib/constants");
+const { getBlsTimeContractClient, getPath, readKeypair } = require("./utils");
+const { PublicKey } = require("@solana/web3.js");
+
+const timeAcceptAdminCommand = new Command("accept-admin")
+  .option(
+    "--cluster <cluster>",
+    "solana cluster: mainnet, testnet, devnet, localnet, <custom>",
+  )
+  .option(
+    "--signer <signer>",
+    "signer: the signer is the pending acount: " + WALLET_PATH,
+  )
+  .description("accept-admin: accept the pending admin of the time contract");
+
+const mint = new Argument(
+  "mint",
+  "mint: the mint is the mint token base58 value ",
+);
+mint.required = true;
+timeAcceptAdminCommand.addArgument(mint).action(async (mint, options) => {
+  options.cluster = options.cluster || "localnet";
+  options.signer = options.signer || getPath(WALLET_PATH);
+  try {
+    const keypair = readKeypair(options.signer);
+    let mintPubkey = null;
+    try {
+      mintPubkey = new PublicKey(mint);
+    } catch (e) {
+      console.log(chalk.red("invaild mint parameter: " + e));
+      process.exit(1);
+    }
+    const client = getBlsTimeContractClient(options.cluster, keypair);
+    await client.blessTimeClient.acceptAdmin();
+    console.log(chalk.green("time contract accept admin success."));
+    process.exit(0);
+  } catch (e) {
+    console.log(chalk.red("accept admin of time contract fail: " + e));
+    process.exit(1);
+  }
+});
+
+module.exports = timeAcceptAdminCommand;
