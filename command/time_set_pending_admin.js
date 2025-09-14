297580f96cb1719b20bd58b3f2a49d9405cf298b
Joinhack <joinhack@gmail.com>
2025-09-14 11:12:28 +0800

    support bless time squads.

--- command/time_set_pending_admin.js
+++ command/time_set_pending_admin.js
@@ -2,7 +2,12 @@ const { Command, Argument } = require("commander");
 const anchor = require("@coral-xyz/anchor");
 const chalk = require("chalk");
 const { WALLET_PATH } = require("../lib/constants");
-const { getBlsTimeContractClient, getPath, readKeypair } = require("./utils");
+const {
+  getBlsTimeContractClient,
+  getPath,
+  readKeypair,
+  bs58Message,
+} = require("./utils");
 const { PublicKey } = require("@solana/web3.js");
 
 const timeSetPendingAdminCommand = new Command("pending-admin")
@@ -15,8 +20,20 @@ const timeSetPendingAdminCommand = new Command("pending-admin")
     "signer: the signer is the admin of the time contract, default: " +
       WALLET_PATH,
   )
+  .option(
+    "--programId <programId>",
+    "Program ID: Specify the program ID when working on devnet, testnet, or localnet; it will not work on mainnet.",
+  )
+  .option(
+    "--squads <true/false>",
+    "squads: if squads true, use squads to signature, default is false.",
+  )
+  .option(
+    "--admin <admin>",
+    "pending: if squads true, use admin to signature in squads",
+  )
   .description(
-    "pending-admin: set/show the pending admin of the registration, the value is base58, if is empty, show the pending admin",
+    "pending-admin: set the pending admin of the registration, the value is base58, if is empty",
   );
 const mint = new Argument(
   "mint",
@@ -27,7 +44,7 @@ const pending = new Argument(
   "pending-admin",
   "pending-admin: the pending admin of the registration",
 );
-pending.required = false;
+pending.required = true;
 timeSetPendingAdminCommand
   .addArgument(mint)
   .addArgument(pending)
@@ -36,7 +53,11 @@ timeSetPendingAdminCommand
     options.signer = options.signer || getPath(WALLET_PATH);
     try {
       const keypair = readKeypair(options.signer);
-      const client = getBlsTimeContractClient(options.cluster, keypair);
+      const client = getBlsTimeContractClient(
+        options.cluster,
+        keypair,
+        options.programId,
+      );
       let mintPubkey = null;
       try {
         mintPubkey = new PublicKey(mint);
@@ -44,12 +65,49 @@ timeSetPendingAdminCommand
         console.log(chalk.red("invaild mint parameter: " + e));
         process.exit(1);
       }
-      if (pending != null) {
-        let pendingAdmin;
-        try {
-          pendingAdmin = new PublicKey(pending);
-        } catch (e) {
-          console.log(chalk.red("invalid pending-admin parameter: " + e));
+
+      const state = await client.blessTimeClient.getTimeState(mintPubkey);
+      let pendingAdmin;
+      try {
+        pendingAdmin = new PublicKey(pending);
+      } catch (e) {
+        console.log(chalk.red("invalid pending-admin parameter: " + e));
+        process.exit(1);
+      }
+      if (options.squads) {
+        const admin = new PublicKey(options.admin);
+        if (state.adminAccount.toBase58() != admin.toBase58()) {
+          console.log(
+            chalk.red(
+              "set merkle pending admin is denied, admin is not matched, the state  admin is " +
+                state.adminAccount.toBase58(),
+            ),
+          );
+          process.exit(1);
+        }
+        const tx = await client.blessTimeClient.getSetPendingAdminAccountTx(
+          mintPubkey,
+          pendingAdmin,
+          {
+            signer: admin,
+          },
+        );
+        const itx = await bs58Message(
+          client.connection,
+          tx.instructions,
+          keypair,
+        );
+        console.log(
+          "bless time set pending admin transaction created: \n" + itx,
+        );
+      } else {
+        if (state.adminAccount.toBase58() != keypair.publicKey.toBase58()) {
+          console.log(
+            chalk.red(
+              "set merkle pending admin is denied, admin is not matched, the state  admin is " +
+                state.adminAccount.toBase58(),
+            ),
+          );
           process.exit(1);
         }
         await client.blessTimeClient.setPendingAdminAccount(
@@ -57,11 +115,6 @@ timeSetPendingAdminCommand
           pendingAdmin,
         );
         console.log(chalk.green("time contract set pending admin success."));
-      } else {
-        const r = await client.blessTimeClient.getTimeState(mintPubkey);
-        console.log(
-          chalk.green("time contract pending admin is " + r.pendingAdmin),
-        );
       }
       process.exit(0);
     } catch (e) {
