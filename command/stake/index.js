37a3cf23961b350310657609b9cd4e305468df66
Joinhack <joinhack@gmail.com>
2025-11-04 11:46:28 +0800

    support pause command for stake

--- command/stake/index.js
+++ command/stake/index.js
@@ -4,6 +4,7 @@ const setPendingAdminCommand = require("./set_pending_admin.js");
 const acceptAdminCommand = require("./accept_admin.js");
 const setAprCommand = require("./set_apr.js");
 const setAprFactorCommand = require("./set_apr_factor.js");
+const setPauseCommand = require("./pause.js");
 
 const stakeCommand = new Command("stake").description(
     "stake: The operations for stake conract.",
@@ -13,6 +14,7 @@ stakeCommand.addCommand(stakeInitializeCommand);
 stakeCommand.addCommand(setPendingAdminCommand);
 stakeCommand.addCommand(setAprFactorCommand);
 stakeCommand.addCommand(acceptAdminCommand);
+stakeCommand.addCommand(setPauseCommand);
 stakeCommand.addCommand(setAprCommand);
 
 module.exports = stakeCommand;
