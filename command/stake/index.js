69ba586ff5343f92dc915b719e11c273413bb981
Joinhack <joinhack@gmail.com>
2025-11-04 22:55:24 +0800

    support unstake command for stake

--- command/stake/index.js
+++ command/stake/index.js
@@ -6,9 +6,10 @@ const setAprCommand = require("./set_apr.js");
 const setAprFactorCommand = require("./set_apr_factor.js");
 const setPauseCommand = require("./pause.js");
 const depositCommand = require("./deposit.js");
+const unstakeCommand = require("./unstake.js");
 
 const stakeCommand = new Command("stake").description(
-    "stake: The operations for stake conract.",
+  "stake: The operations for stake conract.",
 );
 
 stakeCommand.addCommand(stakeInitializeCommand);
@@ -17,6 +18,7 @@ stakeCommand.addCommand(setAprFactorCommand);
 stakeCommand.addCommand(acceptAdminCommand);
 stakeCommand.addCommand(setPauseCommand);
 stakeCommand.addCommand(depositCommand);
+stakeCommand.addCommand(unstakeCommand);
 stakeCommand.addCommand(setAprCommand);
 
 module.exports = stakeCommand;
